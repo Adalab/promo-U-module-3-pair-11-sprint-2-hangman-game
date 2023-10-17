90a59e305965aad9f722eace34df67f1d5f13e20
Irene García <irenegwodak@gmail.com>
2023-10-17 11:22:48 +0200

    Start adding props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import Header from './Header';
+import Dummy from './Dummy';
+import SolutionLetters from './SolutionLetters';
+
 
 // api
-import getWordFromApi from "../services/api";
+import getWordFromApi from '../services/api';
 // styles
-import "../styles/App.scss";
-import "../styles/Dummy.scss";
-import "../styles/Letters.scss";
-import "../styles/Form.scss";
-import "../styles/Header.scss";
+import '../styles/App.scss';
+import '../styles/Dummy.scss';
+
+import '../styles/Form.scss';
+import '../styles/Header.scss';
 
 function App() {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState('');
   const [userLetters, setUserLetters] = useState([]);
-  const [lastLetter, setLastLetter] = useState("");
+  const [lastLetter, setLastLetter] = useState('');
 
   useEffect(() => {
     getWordFromApi().then((word) => {
@@ -29,7 +33,7 @@ function App() {
 
   const handleChange = (ev) => {
     let re = /^[a-zA-ZñÑá-úÁ-Ú´]$/; //add regular pattern
-    if (re.test(ev.target.value) || ev.target.value === "") {
+    if (re.test(ev.target.value) || ev.target.value === '') {
       handleLastLetter(ev.target.value);
     }
   };
@@ -45,17 +49,7 @@ function App() {
     return errorLetters.length;
   };
 
-  const renderSolutionLetters = () => {
-    const wordLetters = word.split("");
-    return wordLetters.map((letter, index) => {
-      const exists = userLetters.includes(letter.toLocaleLowerCase());
-      return (
-        <li key={index} className="letter">
-          {exists ? letter : ""}
-        </li>
-      );
-    });
-  };
+  
 
   const renderErrorLetters = () => {
     const errorLetters = userLetters.filter(
@@ -83,15 +77,11 @@ function App() {
 
   return (
     <div className="page">
-      <header>
-        <h1 className="header__title">Juego del ahorcado</h1>
-      </header>
+      <Header />
       <main className="main">
         <section>
-          <div className="solution">
-            <h2 className="title">Solución:</h2>
-            <ul className="letters">{renderSolutionLetters()}</ul>
-          </div>
+          <SolutionLetters word={word} userLetters={userLetters}/>
+          
           <div className="error">
             <h2 className="title">Letras falladas:</h2>
             <ul className="letters">{renderErrorLetters()}</ul>
@@ -114,21 +104,8 @@ function App() {
             />
           </form>
         </section>
-        <section className={`dummy error-${getNumberOfErrors()}`}>
-          <span className="error-13 eye"></span>
-          <span className="error-12 eye"></span>
-          <span className="error-11 line"></span>
-          <span className="error-10 line"></span>
-          <span className="error-9  line"></span>
-          <span className="error-8  line"></span>
-          <span className="error-7  line"></span>
-          <span className="error-6  head"></span>
-          <span className="error-5  line"></span>
-          <span className="error-4  line"></span>
-          <span className="error-3  line"></span>
-          <span className="error-2  line"></span>
-          <span className="error-1  line"></span>
-        </section>
+        <Dummy number={getNumberOfErrors()}/>
+       
       </main>
     </div>
   );
---
+++ src/components/plantilla.jsx
@@ -0,0 +1,4 @@
+function plantilla() {
+  return <></>;
+}
+export default plantilla;
